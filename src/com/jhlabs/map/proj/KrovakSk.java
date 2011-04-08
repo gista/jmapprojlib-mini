1a5c485fa7684d2b56d751a9796fd491d005f2e4
Marcel Dancak <marcel.dancak@gista.sk>
2011-04-08 14:04:10 +0200

    * code cleaning/optimalization - do not compute some constants for every conversion

--- src/com/jhlabs/map/proj/KrovakSk.java
+++ src/com/jhlabs/map/proj/KrovakSk.java
@@ -16,7 +16,9 @@ import com.jhlabs.map.Ellipsoid;
 
 public class KrovakSk extends Projection {
 
-	static double SEC_TO_RAD = 4.84813681109535993589914102357e-6;
+	public static double SEC_TO_RAD = 4.84813681109535993589914102357e-6;
+	public static final double s45 = 0.785398163397448;
+	public static final double s90 = 1.5707963267948959;
 
 	public boolean tczech = false;
 
@@ -31,6 +33,14 @@ public class KrovakSk extends Projection {
 
 	double m = 0.0;
 
+	private double n0;   // A
+	private double alfa; // B
+	private double u0;   // gama0
+	private double k;    // t0
+	private double s0 = 1.37008346281555; // phi1
+	private double n = sin(s0); // n
+	private double ad = s90 - 1.04216856380474; //(true) azimuth of initial line passing through the projection center
+
 	private Point3D lph = new Point3D();
 	private Point3D xyz = new Point3D();
 
@@ -38,59 +48,53 @@ public class KrovakSk extends Projection {
 		setEllipsoid(Ellipsoid.BESSEL);
 	}
 
+	@Override
+	public void initialize() {
+		super.initialize();
+		// projectionLatitude is fi0
+		alfa = sqrt(1.0 + (es * pow(cos(projectionLatitude), 4)) / (1.0 - es)); // B
+		u0 = asin(sin(projectionLatitude) / alfa); // gama0
+		n0 = a * sqrt(1.0 - es) / (1.0 - es * pow(sin(projectionLatitude), 2)); // A
+		double g = pow((1.0 + e * sin(projectionLatitude)) / (1.0 - e * sin(projectionLatitude)), alfa * e / 2.0);
+		k = tan(u0 / 2.0 + s45) / pow(tan(projectionLatitude / 2.0 + s45), alfa) * g; // t0
+	}
+
+	public final Point2D project(double lam, double phi, Point2D xy) {
+		return transform(new Point2D(lam*RTD, phi*RTD), xy);
+	}
 	/**
 	 * 
 	 * @param lp
-	 *            x - longitude (lambda) y - latitude (phi)
+	 *            x - longitude (lambda)
+	 *            y - latitude (phi)
 	 * @param xy
 	 */
 	public final Point2D transform(Point2D lp, Point2D xy) {
 		//long t1 = System.currentTimeMillis();
-
-		double s45, s90, e2, e, alfa, u0, g, k, n0, ro0, ad, a, s0, n;
-		double gfi, u, fi0, v, s, d, eps, ro;
 		double phi, lam;
-		
-		phi = lp.y*DTR;
+		double ro0, gfi, u, v, s, d, eps, ro;
+
 		lam = lp.x*DTR;
+		phi = lp.y*DTR;
 
 		// apply towgs84 datum shift
 		lph.set(lam, phi, 0);
 		Ellipsoid.WGS_1984.toGeocentric(lph, xyz);
 		shiftDatum(xyz);
 		ellipsoid.toGeodetic(xyz, lph);
-		phi = lph.y;
 		lam = lph.x;
+		phi = lph.y;
 		
-		s45 = 0.785398163397448;
-		s90 = 2 * s45;
-		fi0 = projectionLatitude;//phi0;
-
-		a = 6377397.155;
-		e2 = 0.006674372230614;
-		e = sqrt(e2);
-
-		alfa = sqrt(1. + (e2 * pow(cos(fi0), 4)) / (1. - e2)); // B
-
-		u0 = asin(sin(fi0) / alfa); // gama0
-		g = pow((1. + e * sin(fi0)) / (1. - e * sin(fi0)), alfa * e / 2.); // in
-
-		k = tan(u0 / 2. + s45) / pow(tan(fi0 / 2. + s45), alfa) * g; // t0
-
-		n0 = a * sqrt(1. - e2) / (1. - e2 * pow(sin(fi0), 2)); // A
-		s0 = 1.37008346281555; // phi1
-		n = sin(s0); // n
 		ro0 = scaleFactor * n0 / tan(s0);
-		ad = s90 - 1.04216856380474; //(true) azimuth of initial line passing through the projection center
-		gfi = pow(((1. + e * sin(phi)) / (1. - e * sin(phi))), (alfa * e / 2.));
+		gfi = pow(((1.0 + e * sin(phi)) / (1.0 - e * sin(phi))), (alfa * e / 2.0));
 
-		u = 2. * (atan(k * pow(tan(phi / 2. + s45), alfa) / gfi) - s45); // U
+		u = 2.0 * (atan(k * pow(tan(phi / 2.0 + s45), alfa) / gfi) - s45); // U
 		v = (projectionLongitude - lam) * alfa;
 
 		s = asin(cos(ad) * sin(u) + sin(ad) * cos(u) * cos(v));
 		d = asin(cos(u) * sin(v) / cos(s));
 		eps = n * d;
-		ro = ro0 * pow(tan(s0 / 2. + s45), n) / pow(tan(s / 2. + s45), n);
+		ro = ro0 * pow(tan(s0 / 2.0 + s45), n) / pow(tan(s / 2.0 + s45), n);
 
 		xy.y = ro * cos(eps);
 		xy.x = ro * sin(eps);
@@ -105,46 +109,10 @@ public class KrovakSk extends Projection {
 		return xy;
 	}
 
-	private void shiftDatum(Point3D xyz) {
-		double s = (-m / 1000000.0) + 1.0;
-		double x2 = -dx + s * ( xyz.x    + rz*xyz.y - ry*xyz.z);
-		double y2 = -dy + s * (-rz*xyz.x + xyz.y    + rx*xyz.z);
-		double z2 = -dz + s * ( ry*xyz.x - rx*xyz.y + xyz.z);
-		xyz.set(x2, y2, z2);
-	}
-
-	private void inverseShiftDatum(Point3D xyz) {
-		double scale = (m / 1000000.0) + 1.0;
-		
-		double x2 = dx + scale * ( xyz.x    - rz*xyz.y + ry*xyz.z);
-		double y2 = dy + scale * ( rz*xyz.x + xyz.y    - rx*xyz.z);
-		double z2 = dz + scale * (-ry*xyz.x + rx*xyz.y + xyz.z);
-		xyz.set(x2, y2, z2);
-	}
-
 	public final Point2D inverseTransform(Point2D xy, Point2D lp) {
-		double s45, s90, fi0, e2, e, alfa, u0, g, k, n0, ro0, ad, a, s0, n;
-		double u, deltav, s, d, eps, ro, fi1, xy0;
+		double ro0, u, deltav, s, d, eps, ro, fi1, xy0;
 
-		s45 = 0.785398163397448;
-		s90 = 2 * s45;
-		fi0 = projectionLatitude;
-
-		a = 6377397.15508;
-		e2 = 0.006674372230614;
-		e = sqrt(e2);
-
-		alfa = sqrt(1. + (e2 * pow(cos(fi0), 4)) / (1. - e2));
-		u0 = asin(sin(fi0) / alfa);
-		g = pow((1. + e * sin(fi0)) / (1. - e * sin(fi0)), alfa * e / 2.);
-
-		k = tan(u0 / 2. + s45) / pow(tan(fi0 / 2. + s45), alfa) * g;
-
-		n0 = a * sqrt(1. - e2) / (1. - e2 * pow(sin(fi0), 2));
-		s0 = 1.37008346281555;
-		n = sin(s0);
 		ro0 = scaleFactor * n0 / tan(s0);
-		ad = s90 - 1.04216856380474;
 
 		xy0 = xy.x;
 		xy.x = xy.y;
@@ -158,7 +126,7 @@ public class KrovakSk extends Projection {
 		ro = sqrt(xy.x * xy.x + xy.y * xy.y);
 		eps = atan2(xy.y, xy.x);
 		d = eps / sin(s0);
-		s = 2. * (atan(pow(ro0 / ro, 1. / n) * tan(s0 / 2. + s45)) - s45);
+		s = 2.0 * (atan(pow(ro0 / ro, 1.0 / n) * tan(s0 / 2.0 + s45)) - s45);
 
 		u = asin(cos(ad) * sin(s) - sin(ad) * cos(s) * cos(d));
 		deltav = asin(cos(s) * sin(d) / cos(u));
@@ -187,6 +155,23 @@ public class KrovakSk extends Projection {
 		return lp;
 	}
 
+	private void shiftDatum(Point3D xyz) {
+		double s = (-m / 1000000.0) + 1.0;
+		double x2 = -dx + s * ( xyz.x    + rz*xyz.y - ry*xyz.z);
+		double y2 = -dy + s * (-rz*xyz.x + xyz.y    + rx*xyz.z);
+		double z2 = -dz + s * ( ry*xyz.x - rx*xyz.y + xyz.z);
+		xyz.set(x2, y2, z2);
+	}
+
+	private void inverseShiftDatum(Point3D xyz) {
+		double scale = (m / 1000000.0) + 1.0;
+		
+		double x2 = dx + scale * ( xyz.x    - rz*xyz.y + ry*xyz.z);
+		double y2 = dy + scale * ( rz*xyz.x + xyz.y    - rx*xyz.z);
+		double z2 = dz + scale * (-ry*xyz.x + rx*xyz.y + xyz.z);
+		xyz.set(x2, y2, z2);
+	}
+
 	public boolean hasInverse() {
 		return true;
 	}
@@ -200,19 +185,32 @@ public class KrovakSk extends Projection {
 		krov.setScaleFactor(0.9999);
 		krov.setProjectionLatitude(0.86393797973719311);
 		krov.setProjectionLongitude(0.43342343091192509);
+		krov.initialize();
 
 		Point2D wgsPos = new Point2D(21.23886386, 49.00096926);
 		Point2D out = new Point2D();
 		krov.transform(wgsPos, out);
 		//krov.transform(new Point2D(16.849771965029522, 50.209011566079397), out);
 		System.out.println(wgsPos);
-		//System.out.println("EXPECTED: [-262731.724122, -1208353.150831]");
+		System.out.println("EXPECTED: [-262731.724126, -1208353.150846]");
 		System.out.println(out);
 		
 		Point2D xy = new Point2D();
 		krov.inverseTransform(out, xy);
 		System.out.println(xy);
 		
-		System.out.println(Ellipsoid.WGS_1984.poleRadius);
+		// test2
+		System.out.println();
+		wgsPos = new Point2D(21.127741, 49.0156973);
+		krov.transform(wgsPos, out);
+		System.out.println(wgsPos);
+		System.out.println("EXPECTED: [-270773.835347, -1206330.287303]");
+		System.out.println(out);
+		krov.inverseTransform(out, xy);
+		System.out.println(xy);
+		
+		Projection proj = ProjectionFactory.getNamedPROJ4CoordinateSystem("epsg:102067");
+		System.out.println(proj);
+		proj.dump();
 	}
 }
